95b9078e841f6df4b898a62e93f61bb688460ebf
Ryszard Grabowski <ryszard.grabowski00@gmail.com>
2023-03-07 19:36:06 +0100

    init project

---
+++ groceries.py
@@ -0,0 +1,9 @@
+my_dict = {"warzywniak" : ["pomidor", "ogórek", "ziemniaki"],
+          "piekarnia" : ["bułki", "chleb", "pączek"]}
+new_dict = {k.capitalize() : [s.capitalize() for s in v] for k, v in my_dict.items()}
+total = 0
+for k, v in new_dict.items():
+    print(f"ide do {k}, kupuje nastepujace rzeczy {', '.join(v)}")
+    total += len(v)
+print(f"w sumie kupiłem {total} rzeczy")
+    
